1c7feb5bc303ea3d11c2150d1300d885585e9a98
FrancescoGuggino <landongu@gmail.com>
2023-07-13 14:24:27 +0200

    add name guest

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,12 +1,26 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
 
         //creare e inizializzare l’array contenente i nomi degli invitati
         String[] guestList = {"Dua Lipa" , "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
 
         //chiedere all’utente come si chiama
+        String nameGuest;
+
+        System.out.println("Inserisci il tuo nome e cognome: ");
+        nameGuest = scan.nextLine();
+
         //verificare che il nome sia presente nella lista
+        for () {
+
+
+
+        }
     }
 }
