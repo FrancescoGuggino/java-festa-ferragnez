1554e7a284dfba217d0926c03d0ebeffa2c00107
FrancescoGuggino <landongu@gmail.com>
2023-07-13 14:02:51 +0200

    add array

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,12 @@
+package com.ferragnez.party;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+
+        //creare e inizializzare l’array contenente i nomi degli invitati
+        String[] guestList = {"Dua Lipa" , "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        //chiedere all’utente come si chiama
+        //verificare che il nome sia presente nella lista
+    }
+}
